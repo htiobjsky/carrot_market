a5fc985396868868df9e737272ef9ef50b9229a9
heetaekim <htiobjsky@gmail.com>
2021-05-03 07:51:20 +0900

    #10

--- lib/page/detail.dart
+++ lib/page/detail.dart
@@ -62,13 +62,119 @@ class _DetailContentViewState extends State<DetailContentView> {
     );
   }
 
-  Widget _bodyWidget() {
-    return Column(
-      children: [
-        _makeSliderImage(),
-        _sellerSimpleInfo(),
-      ],
+  Widget _line() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      height: 1,
+      color: Colors.grey.withOpacity(0.3),
     );
+  }
+
+  Widget _contentDetail() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: 20),
+          Text(
+            widget.data['title'],
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "디지털/가전 ∙ 22시간 전",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "선물받은 새상품이고\n상품 꺼내보기만 했습니다\n거래는 직거래만 합니다.",
+            style: TextStyle(fontSize: 15, height: 1.5),
+          ),
+          SizedBox(height: 15),
+          Text(
+            "채팅 3 ∙ 관심 17 ∙ 조회 295",
+            style: TextStyle(
+              fontSize: 12,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 15),
+        ],
+      ),
+    );
+  }
+
+  Widget _otherCellContents() {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            "판매자님의 판매 상품",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "모두 보기",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _bodyWidget() {
+    return CustomScrollView(slivers: [
+      SliverList(
+        delegate: SliverChildListDelegate(
+          [
+            _makeSliderImage(),
+            _sellerSimpleInfo(),
+            _line(),
+            _contentDetail(),
+            _line(),
+            _otherCellContents(),
+          ],
+        ),
+      ),
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        sliver: SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 5,
+            crossAxisSpacing: 5,
+          ),
+          delegate: SliverChildListDelegate(List.generate(20, (index){
+            return Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ClipRRect(
+                      child: Container(color: Colors.grey, height: 120,),
+                      borderRadius: BorderRadius.circular(10),
+                  ),
+                  Text("상품 제목", style: TextStyle(fontSize: 14),),
+                  Text("금액", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                ],
+              ),
+            );
+          }).toList()),
+        ),
+      )
+    ]);
   }
 
   Widget _sellerSimpleInfo() {
@@ -89,15 +195,23 @@ class _DetailContentViewState extends State<DetailContentView> {
             backgroundImage: Image.asset('assets/images/user.png').image,
             // child: Image.asset('assets/images/user.png'),
           ),
-          SizedBox(width: 10,),
+          SizedBox(
+            width: 10,
+          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("BJSKY", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),),
+              Text(
+                "BJSKY",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
               Text("서울시 반포동"),
             ],
           ),
-          Expanded(child: ManorTemperature(manorTemp: 37.5,)),
+          Expanded(
+              child: ManorTemperature(
+            manorTemp: 37.5,
+          )),
         ],
       ),
     );
